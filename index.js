f6878e515f082dc88a0283542399d172b2805494
Nguyen Quang Huy <huy19712011@gmail.com>
2020-11-09 15:13:18 +0700

    EventEmitter, event arguments

---
+++ index.js
@@ -0,0 +1,13 @@
+const EventEmitter = require('events');
+
+const emitter = new EventEmitter();
+
+emitter.on('messageLogged', (e) => {
+  console.log('Listener called...', e);
+});
+
+emitter.emit('messageLogged', {id: 1, url: 'http://'});
+
+
+
+
